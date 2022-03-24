4a0d04bbe7a913437851fcee8d0bc57a7d3fb12a
julian <illegal_burrito@live.com>
2022-03-24 14:09:23 -0700

    first commit

---
+++ public/demo.js
@@ -0,0 +1,1 @@
+$('#root').text('Demo is Working!');
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = document.getElementById('root');
+const component = <div>React and webpack are working</div>
+
+ReactDOM.render(component, root);
